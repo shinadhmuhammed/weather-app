f37e4bae8dac3a75e7e6f53b2286e06a9e4dc2bc
Your Name <Naseef K>
2024-06-23 00:19:41 +0530

    UI Part of weather app completed

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './Weather.css'
 import searchIcon from '../assets/search-icon.jpg'
+import weather from '../assets/weather1.webp'
+import Humidity from '../assets/humidity.jpg'
+import windSpeed from '../assets/windSpeed.jpg'
 
 
 const Weather = () => {
@@ -11,7 +14,26 @@ const Weather = () => {
                 <img src={searchIcon} alt='search' className='img'/>
             </div>
 
+            <img src={weather} alt='weather' className='weather-icon'/>
+            <p className='temperature'>16C</p>
+            <p className='location'>London</p>
 
+            <div className="weather-data">
+                <div className="col">
+                    <img src={Humidity } alt='Humidity' className='img-humidity'/>
+                    <div>
+                        <p>91%</p>
+                        <span>Humidity</span>
+                    </div>
+                </div>
+                <div className="col">
+                    <img src={windSpeed } alt='windSpeed ' className='windSpeed'/>
+                    <div>
+                        <p>3.6 Km/h</p>
+                        <span>Wind Speed</span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
